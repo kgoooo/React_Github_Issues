0620db12cffd94c216d0a09327b139dc96a2b199
Kristen Goehringer <kgoehringer@gmail.com>
2017-10-25 19:49:12 -0400

    Partially wired Issue list & issue component.  Pre simplify props passed in.

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -1,19 +1,47 @@
 import React from 'react';
 
+// const Issue = (props) => (
+// 	<div className="issue-wrapper">
+// 		<div>
+// 			{props.data.data ? <h4>{props.data.data.items[0].title}</h4> : <h4>Title</h4>}
+// 		</div>
+// 		<div className="issue-details-wrapper">
+// 			<div className="issue-details">
+// 				{props.data.data ? <p>Issue #{props.data.data.items[0].number}</p> : <p>Issue #999</p>}
+// 				{props.data.data ? <p>Created at {props.data.data.items[0].created_at}</p> : <p>Created at 2017-10-25T02:24:25Z</p>}
+// 				{props.data.data ? <p><strong>Username: {props.data.data.items[0].user.login}</strong></p> : <p><strong>Username</strong></p>}				
+				
+// 			</div>
+// 			<div>
+// 				{props.data.data ? <p>{props.data.data.items[0].comments} Comments</p> : <p>Comments: 69</p>}			
+// 			</div>
+// 		</div>
+// 		<div>
+// 			{props.data.data ? <p>{props.data.data.items[0].labels.length} Labels</p> : <p>22 Labels</p>}			
+// 		</div>
+// 	</div>
+// )
+
+// export default Issue;
+
 const Issue = (props) => (
 	<div className="issue-wrapper">
 		<div>
-			<h4>Title</h4>
+			{props.data.data ? <h4>{props.data.data.items[0].title}</h4> : <h4>Title</h4>}
 		</div>
 		<div className="issue-details-wrapper">
 			<div className="issue-details">
-				<p>Issue #349</p>
-				<p>Created 39 days ago</p>
-				<p><strong>Username</strong></p>
+				{props.data.data ? <p>Issue #{props.data.data.items[0].number}</p> : <p>Issue #999</p>}
+				{props.data.data ? <p>Created at {props.data.data.items[0].created_at}</p> : <p>Created at 2017-10-25T02:24:25Z</p>}
+				{props.data.data ? <p><strong>Username: {props.data.data.items[0].user.login}</strong></p> : <p><strong>Username</strong></p>}
+
 			</div>
 			<div>
-				<p>20 Comments +</p>
+				{props.data.data ? <p>{props.data.data.items[0].comments} Comments</p> : <p>Comments: 69</p>}
 			</div>
+		</div>
+		<div>
+			{props.data.data ? <p>{props.data.data.items[0].labels.length} Labels</p> : <p>22 Labels</p>}
 		</div>
 	</div>
 )
--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import Issue from './Issue';
+import GithubIssueFinderApp from './GithubIssueFinderApp';
 
-const IssueList = (props) => (
-	<div>
-		<Issue data={props}/>
-		{props.items}
-	</div>
-)
+// const IssueList = (props) => (
+// 	<div>
+// 		<Issue data={props}/>
+// 	</div>
+// )
+
+// export default IssueList;
+
+class IssueList extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	mapIssuesToList = (props) => {
+		for(var i = 0; i < 5; i++){
+			const title = this.props.data.items[i].title;
+			const issueNumber = this.props.data.items[i].number;
+			const createdAt = this.props.data.items[i].created_at;
+			const userLogin = this.props.data.items[i].user.login;
+			const comments = this.props.data.items[i].comments;
+			console.log("title: " + title + " - # " + issueNumber);
+		}
+	}
+	render(){
+		return(
+			<div>
+				<button onClick={this.mapIssuesToList} >CLICK</button>
+				<Issue data={this.props} />
+			</div>
+		)
+	}
+}
 
 export default IssueList;
